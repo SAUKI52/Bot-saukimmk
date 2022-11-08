6425d8fe73376dfb3c1f7728b4cd0afd82332636
Reiibotz <topuprainalz@gmail.com>
2022-11-08 12:52:50 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,65 @@
+# Example bot from Rest APIs
+<a href="https://danzzapi.xyz">danzzapi.xyz</a>
+
+## Intalasi
+### For termux
+
+```
+> apt update && apt upgrade
+> pkg install git
+> pkg install nodejs
+> pkg install ffmpeg
+> pkg install imagemagick
+> pkg install libwebp
+> pkg install yarn
+> git clone https://github.com/Danzzxcodes/basebot-v1
+> cd basebot-v1
+> yarn
+> npm start
+```
+
+## Heroku buildpack
+
+```FFMPEG :```
+https://github.com/Danzzxcodes/heroku-buildpack-ffmpeg-latest.git
+
+```WEBP :```
+https://github.com/Danzzxcodes/heroku-buildpack-webp-binaries.git
+
+---------
+
+## For windows/RDP
+
+* Download And Install Git [`Click Here`](https://git-scm.com/downloads)
+* Download And Install NodeJS [`Click Here`](https://nodejs.org/en/download)
+* Download And Install FFMPEG [`Click Here`](https://ffmpeg.org/download.html)
+* Download And Install ImageMagick [`Click Here`](https://imagemagick.org/script/download.php)
+
+```
+> git clone https://github.com/Danzzxcodes/basebot-v1
+> cd basebot-v1
+> npm i
+> npm start
+```
+
+
+## Features list
+* Downloader
+* Asupan
+* Cecan
+* Cogan
+* Search
+* Text pro
+* Photo oxy
+* Ephoto
+* convert
+* Others
+* Owner
+
+## Social media
+<a href="https://youtube.com/DanzzCoding">Youtube</a><br>
+<a href="https://instagram.com/ramdani_real01">Instagram</a><br>
+
+## Contact
+<a href="https://wa.me/6289512545999">WhatsApp</a><br>
+<a href="mailto:danzzcoding@gmail.com">Email</a>
---
+++ set.js
@@ -0,0 +1,54 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+global.APIs = {
+	danzz: 'https://danzzapi.xyz',
+}
+
+global.APIKeys = {
+	'https://danzzapi.xyz': 'danzz',
+}
+
+// Other
+global.apikey = ['YOUR APIKEY'] // Get in: https://danzzapi.xyz
+global.apikeyprem = ['YOUR APIKEY'] // Get in: https://danzzapi.xyz
+global.apikeyvip = ['YOUR APIKEY'] // Get in: https://danzzapi.xyz
+global.namabot = ['SAUKI OFFICIAL']
+global.namaowner = ['SAUKI OFFICIAL']
+global.owner = ['62895357313987','6287778405247']
+global.premium = ['62895357313987','6287778405247']
+global.packname = '© Powered By'
+global.author = 'Reii Dev'
+global.sessionName = 'session'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.prefix = ['']
+global.symbol1 = '•'
+global.symbol2 = '>'
+global.wm = '© Reiinalz'
+global.mess = {
+    success: 'Success ✓',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Khusus Group Chat',
+    private: 'Fitur Khusus Private Chat!',
+    bot: 'Fitur Khusus Nomor Bot',
+    wait: 'Waittt...',
+    premium: 'Kamu Bukan User Premium, Beli Sana Ke Owner Bot',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Pukul 00:00 WIB.',
+}
+global.limitawal = {
+	free: "100",
+    premium: "1000",
+    vip: "unlimited"
+}
+global.thumb = fs.readFileSync('./media/img/1.jpg')
+global.qris = { url: 'https://telegra.ph/file/cd0372ebf13b265f3feb5.jpg' }
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.greenBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
